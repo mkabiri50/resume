5b79d773cb6f26618bb2ecd34f914fd032a974fe
mahdi kabiri <mkabiri50@yahoo.com>
2019-07-04 18:15:54 +0430

    Updates

---
+++ precache-manifest.218e3eacd1f9a3c921b7fff6a50062dc.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8049dc0a985e893cfbce5c2aed52583",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "f15d06622ccc2057a3c6",
+    "url": "/resume/static/css/2.cceee878.chunk.css"
+  },
+  {
+    "revision": "3ee8b02bac0fbda7ca15",
+    "url": "/resume/static/css/main.c01d5c89.chunk.css"
+  },
+  {
+    "revision": "f15d06622ccc2057a3c6",
+    "url": "/resume/static/js/2.b7532c5b.chunk.js"
+  },
+  {
+    "revision": "3ee8b02bac0fbda7ca15",
+    "url": "/resume/static/js/main.56709484.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "7d4fe56a906707466de4e066e8269719",
+    "url": "/resume/static/media/css.7d4fe56a.png"
+  },
+  {
+    "revision": "52954671523fac16dce81667234529cb",
+    "url": "/resume/static/media/ed.52954671.jpeg"
+  },
+  {
+    "revision": "e73667c4a2c3040b3057be9b7865a919",
+    "url": "/resume/static/media/js.e73667c4.png"
+  },
+  {
+    "revision": "55e83371dbf50a5f027b1fe31a054ad3",
+    "url": "/resume/static/media/nodejs.55e83371.png"
+  },
+  {
+    "revision": "6f0cc7e3df430f03c62cbbd3375b2cce",
+    "url": "/resume/static/media/react.6f0cc7e3.png"
+  },
+  {
+    "revision": "0f820576af0e0cba562cb9badf806c1d",
+    "url": "/resume/static/media/reactnative.0f820576.png"
+  },
+  {
+    "revision": "4912f6d800e1b75354e0b5d4794e1856",
+    "url": "/resume/static/media/redux.4912f6d8.png"
+  },
+  {
+    "revision": "d47cbd4fc6a750a8735fb5c1b94aabaa",
+    "url": "/resume/static/media/winter-1.d47cbd4f.jpg"
+  },
+  {
+    "revision": "4f7b679799eafeeb1e70a989f67dfee6",
+    "url": "/resume/static/media/winter-2.4f7b6797.jpg"
+  },
+  {
+    "revision": "aebc82e6df3d1e48bc42d7356b48de7b",
+    "url": "/resume/static/media/winter-3.aebc82e6.jpg"
+  },
+  {
+    "revision": "0bb012f7a2201be73edf3846d0f8a8fd",
+    "url": "/resume/static/media/winter-4.0bb012f7.jpg"
+  }
+]);
